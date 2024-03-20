ee0585de1274fe52c3b553287be5c3fe8f5511bd
Pedro Henrique <you@example.com>
2024-03-20 00:48:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/assets/avatar.png")
+  }
+}
